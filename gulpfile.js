22e3c73b9693f247257d3946a39f17ea62023881
Aliaksei Makuch <aliaksei.makuch@cogniteq.com>
2019-01-28 15:27:07 +0300

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,7 @@ const gulp = require("gulp");
 const config = require("./env.paths.json");
 const env = process.env.NODE_ENV;
 
-// плагины галпа отдельно подключать не нужно,
-// используем в пайпе как $gp.имяПлагина (без приставки gulp-)
+
 const $gp = require("gulp-load-plugins")();
 
 const browserSync = require("browser-sync").create();
@@ -12,7 +11,7 @@ const $webpack = require("webpack-stream");
 const webpack = require("webpack");
 const del = require("del");
 
-// стили
+
 gulp.task("styles", () => {
   return gulp
     .src(`${config.SRC_DIR}/styles/main.scss`)
@@ -25,19 +24,19 @@ gulp.task("styles", () => {
     .pipe(reload({ stream: true }));
 });
 
-// переносим шрифты
+
 gulp.task("fonts", () => {
   return gulp
     .src(`${config.SRC_DIR}/fonts/**`)
     .pipe(gulp.dest(`${config.DIST_DIR}/assets/fonts/`));
 });
 
-// очистка
+
 gulp.task("clean", () => {
   return del(config.ROOT_PATH);
 });
 
-// собираем скрипты webpack
+
 gulp.task("scripts", () => {
   return gulp
     .src(`${config.SRC_DIR}/scripts/*.js`)
@@ -55,7 +54,7 @@ gulp.task("scripts", () => {
     .pipe(reload({ stream: true }));
 });
 
-//рендерим странички
+
 gulp.task("pug", () => {
   return gulp
     .src(`${config.VIEWS_DIR}/pages/*.pug`)
@@ -65,7 +64,7 @@ gulp.task("pug", () => {
     .pipe(reload({ stream: true }));
 });
 
-// dev сервер + livereload (встроенный)
+
 gulp.task("server", () => {
   browserSync.init({
     server: {
@@ -75,7 +74,7 @@ gulp.task("server", () => {
   });
 });
 
-// спрайт иконок
+
 gulp.task("svg", done => {
   return gulp
     .src(`${config.SRC_DIR}/images/icons/*.svg`)
@@ -113,7 +112,7 @@ gulp.task("svg", done => {
     .pipe(gulp.dest(`${config.DIST_DIR}/assets/images/icons`));
 });
 
-// просто переносим картинки
+
 gulp.task("images", () => {
   return gulp
     .src([
@@ -123,7 +122,7 @@ gulp.task("images", () => {
     .pipe(gulp.dest(`${config.DIST_DIR}/assets/images/`));
 });
 
-// галповский вотчер
+
 gulp.task("watch", () => {
   gulp.watch(`${config.SRC_DIR}/styles/**/*.scss`, gulp.series("styles"));
   gulp.watch(`${config.SRC_DIR}/images/**/*.*`, gulp.series("images"));
@@ -132,7 +131,7 @@ gulp.task("watch", () => {
   gulp.watch(`${config.VIEWS_DIR}/**/*.pug`, gulp.series("pug"));
 });
 
-// GULP:DEV
+
 gulp.task(
   "default",
   gulp.series(
@@ -143,7 +142,7 @@ gulp.task(
   )
 );
 
-// GULP:build
+
 gulp.task(
   "build",
   gulp.series(
